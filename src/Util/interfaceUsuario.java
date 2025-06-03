20e1d904a939680324bcbeed0cee9c7179d4001b
Octavio <octaviocc0161@gmail.com>
2025-06-03 14:20:21 -0300

    Semana 3 - versão final

--- src/Util/interfaceUsuario.java
+++ src/Util/interfaceUsuario.java
@@ -14,8 +14,8 @@ public class interfaceUsuario {
         System.out.print("Qual o valor do imovel? R$: ");
         double valor = scanner.nextDouble();
 
-        while (valor <= 0) {
-            System.out.println("O valor não pode ser igual ou menor que 0! Tente novamente.");
+        while (valor <= 0 || valor > 1_000_000_000) {
+            System.out.println("Valor inválido! Tente novamente.");
             System.out.println("Qual o valor do imovel? R$:");
             valor = scanner.nextDouble();
         }
@@ -26,8 +26,8 @@ public class interfaceUsuario {
         System.out.println("Qual o prazo do financiamento em anos? ");
         int prazo = scanner.nextInt();
 
-        while (prazo <= 0) {
-            System.out.println("O prazo não pode ser igual ou menor que 0! Tente novamente.");
+        while (prazo <= 0 || prazo > 50) {
+            System.out.println("Prazo inválido! Tente novamente.");
             System.out.println("Qual o prazo do financiamento em anos? ");
             prazo = scanner.nextInt();
         }
@@ -38,8 +38,8 @@ public class interfaceUsuario {
         System.out.println("Qual a taxa de juros anual (em %)? ");
         double taxa = scanner.nextDouble();
 
-        while (taxa <= 0) {
-            System.out.println("A taxa de juros não pode ser igual ou menor que 0! Tente novamente.");
+        while (taxa <= 0 || taxa > 100) {
+            System.out.println("Taxa de juros inválida! Tente novamente.");
             System.out.println("Qual a taxa de juros anual (em %)? ");
             taxa = scanner.nextDouble();
         }
